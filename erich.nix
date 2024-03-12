3ad7089db131d87be7d8eef32a411e4569fe05af
Erich Ellsworth <erich@kg5key.com>
2024-03-11 19:46:34 -0500

    23 current  2024-03-11 19:45:19  24.05pre590113.1536926ef562  6.6.18                          *

---
+++ erich.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    <home-manager/nixos>
+  ];
+
+  # Allow unfree packages.
+  nixpkgs.config.allowUnfree = true;
+
+  users.users.erich = {
+    isNormalUser = true;
+    description = "Erich Ellsworth";
+    extraGroups = ["networkmanager" "wheel" "docker" "dialout"];
+    packages = with pkgs; [
+      firefox
+      kate
+      alejandra
+      #  thunderbird
+    ];
+    shell = pkgs.nushell;
+  };
+
+  home-manager.users.erich = {pkgs, ...}: {
+    home.packages = [
+      pkgs.arduino-ide
+      pkgs.git
+      pkgs.python3
+      pkgs.psst
+      #pkgs.discord
+      pkgs.bitwarden
+
+      # Programming Languages
+      pkgs.gforth
+
+      ## Rust
+      pkgs.rustc
+      pkgs.cargo
+      pkgs.rust-analyzer
+
+      ## Editors
+      #      pkgs.vscode
+      pkgs.helix
+
+      pkgs.octaveFull
+
+      # Games
+      pkgs.prismlauncher
+    ];
+
+    programs.bash.enable = true;
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "23.11";
+  };
+}
