26bbb28e14e859132914ddf88e36351f5fdfcdb7
Erich Ellsworth <kg5key@kg5key.com>
2024-04-06 22:40:38 -0500

    apollo - 27 current  2024-04-06 22:13:53  23.11.5871.72da83d9515b  6.1.84                          *

--- apollo/apollo.nix
+++ apollo/apollo.nix
@@ -121,6 +121,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  programs.ssh.setXAuthLocation = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [22];
