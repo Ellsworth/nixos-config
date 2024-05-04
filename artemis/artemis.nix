4bc75a5ee429cd27aeeef8126f82698021185cc5
Erich Ellsworth <erich@kg5key.com>
2024-05-03 19:04:12 -0500

    artemis - 64 current  2024-05-03 19:04:06  24.05pre619660.63c3a29ca824  6.6.29                          *

--- artemis/artemis.nix
+++ artemis/artemis.nix
@@ -70,8 +70,8 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
+  services.xserver.desktopManager.plasma5.enable = true;
+  #services.desktopManager.plasma6.enable = true;
   # Hyprland WM
   programs.hyprland.enable = true;
 
