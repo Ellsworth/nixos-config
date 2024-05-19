5455f358ca0935ff58668a34885913404f24d9b7
Erich Ellsworth <kg5key@kg5key.com>
2024-05-19 15:24:27 -0500

    remote build support

---
+++ modules/remote-build-client.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  nix.buildMachines = [
+    {
+      hostName = "artemis";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      # if the builder supports building for multiple architectures,
+      # replace the previous line by, e.g.
+      # systems = ["x86_64-linux" "aarch64-linux"];
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = ["big-parallel" "kvm"];
+      mandatoryFeatures = [];
+    }
+  ];
+  nix.distributedBuilds = true;
+  # optional, useful when the builder has a faster internet connection than yours
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+  '';
+
+  nix.settings.trusted-public-keys = ["artemis:Y4Bz+sblcfwMbaSb9OoPZ5OM5Xh8KGS3S8zuhkMJSCk="];
+}
