58d52030537546940ac49bcb82e60953bbb92878
Erich Ellsworth <kg5key@kg5key.com>
2024-08-31 15:15:45 -0500

    Update Apollo's SSH key

--- erich.nix
+++ erich.nix
@@ -77,7 +77,7 @@
   users.users."erich".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3yEyI+ih4/rc4tNcXOImlUUCMJ1n/h6DpjXTBAyiL9 artemis"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSVvYsapiP3wSXptz3D3y3VRtpB1SS8Os4Gfk5g4xaT ceres"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILQ27ZwmB/SkEB/zfg3LuJJ/PMEyCGTz9qUfgsTUnoJ1 apollo"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgrQ68WfJgDpLPNCESP8ZuBpE13+C36Y1HVQ8u71bCT apollo"
   ];
 
   # Chrony NTP Service
