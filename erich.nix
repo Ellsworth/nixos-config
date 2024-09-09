35bb68894254ffe584b6557a71cc90dc31bdf299
Erich Ellsworth <erich@kg5key.com>
2024-09-09 13:34:57 -0500

    apollo - 18 current  2024-09-09 13:34:53  24.11pre677397.574d1eac1c20  6.6.49                          *

--- erich.nix
+++ erich.nix
@@ -83,12 +83,14 @@
       autoPrune.enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      #dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
   };
+
+  virtualisation.docker.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh = {
