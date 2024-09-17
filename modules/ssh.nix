adc8bcaab6774e4dc1c00c0c51414747aca66bc6
Erich Ellsworth <erich@kg5key.com>
2024-09-16 23:15:43 -0500

    apollo - 26 current  2024-09-16 23:14:22  24.11pre677397.574d1eac1c20  6.6.49                          *

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -20,4 +20,22 @@
       };
     };
 
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
+  programs.ssh.setXAuthLocation = true;
+
+  # SSH keys
+  users.users."erich".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3yEyI+ih4/rc4tNcXOImlUUCMJ1n/h6DpjXTBAyiL9 artemis"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSVvYsapiP3wSXptz3D3y3VRtpB1SS8Os4Gfk5g4xaT ceres"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgrQ68WfJgDpLPNCESP8ZuBpE13+C36Y1HVQ8u71bCT apollo"
+  ];
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedUDPPorts = [ 22 ];
+
 }
