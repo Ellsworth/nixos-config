78eb2803dca0a5123a6a34c12821be3cceefa9f9
Erich Ellsworth <erich@kg5key.com>
2024-10-03 20:04:47 -0500

    artemis - 126 current  2024-10-03 20:04:43  24.11pre687768.27e30d177e57  6.11.1                           *

--- erich.nix
+++ erich.nix
@@ -122,7 +122,7 @@
     servers = [ "pool.ntp.org" "time.nist.gov" "100.75.40.55" "100.82.239.88" ];
     extraConfig = ''
       makestep 0.1 1
-      allow 100.0.0.0/24
+      allow 100.0.0.0/8
     '';
   };
 
