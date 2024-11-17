fb5cc7287febeb4dbbe1152c5c1500b96cfe4462
Erich Ellsworth <erich@kg5key.com>
2024-11-17 14:53:20 -0600

    artemis - 5 current   2024-11-17 14:12:53  25.05beta708622.5e4fbfb6b3de  6.11.8                          *

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.artemis {
+        system = "x86_64-linux";
+        modules = [
+          ./machines/artemis.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+  };
+}
