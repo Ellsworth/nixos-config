0b5a3dcff8f5a2c1a20456625ad37ee421dfdf3e
Erich Ellsworth <erich@kg5key.com>
2024-11-19 07:45:48 -0600

    apollo - 55 current  2024-11-18 14:22:22  24.11pre706477.dc460ec76cbf  6.6.60                          *

--- flake.nix
+++ flake.nix
@@ -3,23 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      hostname = nixpkgs.lib.artemis {
-        system = "x86_64-linux";
-        modules = [
-          ./machines/artemis.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
-        ];
+  outputs =
+    inputs@{ self, nixpkgs, home-manager, nixos-hardware, ... }:
+    {
+      nixosConfigurations = {
+        hostname = nixpkgs.lib.artemis {
+          system = "x86_64-linux";
+          modules = [
+            ./machines/artemis.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+          ];
+        };
+        hostname = nixpkgs.lib.apollo {
+          system = "x86_64-linux";
+          modules = [
+            ./machines/apollo.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+            nixos-hardware.nixosModules.dell-xps-13-9380
+          ];
+        };
       };
     };
-  };
 }
