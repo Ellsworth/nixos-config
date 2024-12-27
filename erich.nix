7f14246b7fb395f51147f8f861c9473f9ab1dc23
Erich Ellsworth <erich@kg5key.com>
2024-12-26 23:08:07 -0600

    apollo - 74 current  2024-12-26 23:08:03  25.05beta724962.d70bd19e0a38  6.6.66                          *

--- erich.nix
+++ erich.nix
@@ -117,12 +117,7 @@
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
-
-    docker = {
-      enable = true;
-      autoPrune.enable = true;
-    };
-
+    
     oci-containers.backend = "podman";
   };
 
--- machines/ceres.nix
+++ machines/ceres.nix
@@ -46,6 +46,11 @@
     "fs.file-max" = 2097152;
   };
 
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
