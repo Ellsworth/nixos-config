3f8cdb07f5165be3bb9852fdd993b92884dbd33a
Erich Ellsworth <erich@kg5key.com>
2024-12-26 19:19:32 -0600

    ceres - 97 current  2024-12-26 19:19:25  25.05beta724962.d70bd19e0a38  6.6.66                          *

--- machines/ceres.nix
+++ machines/ceres.nix
@@ -41,6 +41,11 @@
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
+  # Solve InfluxDB related errors involving "too many open files."
+  boot.kernel.sysctl = {
+    "fs.file-max" = 2097152;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
