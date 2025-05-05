c7b6d4fc019190a22438854675486f66a5ec33ae
Erich Ellsworth <erich@kg5key.com>
2025-05-04 21:49:55 -0400

    apollo – 133 current  2025-05-04 21:49:51  25.05.20250504.979daf3       6.12.26                          *

--- modules/vm.nix
+++ modules/vm.nix
@@ -26,6 +26,7 @@
         swtpm.enable = true;
         ovmf.enable = true;
         ovmf.packages = [ pkgs.OVMFFull.fd ];
+        vhostUserPackages = with pkgs; [ virtiofsd ];
       };
     };
     spiceUSBRedirection.enable = true;
