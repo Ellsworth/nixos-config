bf00c8686ae1890f97164e73ace527970fe66d05
Erich Ellsworth <erich@kg5key.com>
2025-11-13 13:19:04 -0600

    apollo - 225 current  2025-11-13 13:19:00  25.05.20251110.c244830  6.12.57                          *

--- machines/apollo.nix
+++ machines/apollo.nix
@@ -28,6 +28,19 @@
     nvtopPackages.amd
   ];
 
+  services.power-profiles-daemon.enable = false;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+
   home-manager.users.erich =
     { pkgs, ... }:
     {
