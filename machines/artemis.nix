7c5b5fd80532e0ba75244dc4e91579cc15499bb1
Erich Ellsworth <erich@kg5key.com>
2025-11-12 20:03:18 -0600

    artemis - 138 current  2025-11-12 20:03:13  25.05.20251110.c244830  6.17.7                           *

--- machines/artemis.nix
+++ machines/artemis.nix
@@ -35,6 +35,9 @@
     };
   };
 
+  # For Lutris?
+  hardware.graphics.enable32Bit = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
