182fea48354657b5e6af24813a63ca8ffc7ee2c9
Erich Ellsworth <erich@kg5key.com>
2026-07-23 20:18:29 -0500

    artemis -

--- machines/hermes.nix
+++ machines/hermes.nix
@@ -54,7 +54,7 @@
 
       # The state version is required and should stay at the version you
       # originally installed.
-      home.stateVersion = "25.05";
+      home.stateVersion = lib.mkForce "25.05";
     };
 
   virtualisation.oci-containers = {
